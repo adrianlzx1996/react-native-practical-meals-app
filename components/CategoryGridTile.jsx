13cbb0f22ccf38390eef1d82758c7a8f2d1a0530
Adrian <leongadrian36@gmail.com>
2022-10-14 11:10:05 +0800

    add onPress

--- components/CategoryGridTile.jsx
+++ components/CategoryGridTile.jsx
@@ -1,9 +1,10 @@
 import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
 	return (
 		<View style={styles.gridItem}>
-			<Pressable style={({ pressed }) => [ styles.button, pressed ? styles.buttonPressed : null ]}
+			<Pressable onPress={onPress}
+					   style={({ pressed }) => [ styles.button, pressed ? styles.buttonPressed : null ]}
 					   android_ripple={{ color: "#ccc" }}>
 				<View style={[ styles.innerContainer, { backgroundColor: color } ]}>
 					<Text style={styles.title}>{title}</Text>
