ae23e83ee76ab05af3c1b4a4a52b39e08fb1eb8c
Adrian <leongadrian36@gmail.com>
2022-10-14 12:22:04 +0800

    scroll view

--- screens/MealDetailsScreen.jsx
+++ screens/MealDetailsScreen.jsx
@@ -1,6 +1,8 @@
-import { Image, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 function MealDetailsScreen({ route }) {
 	const mealId = route.params.mealId;
@@ -8,20 +10,48 @@ function MealDetailsScreen({ route }) {
 	const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
 	return (
-		<View>
-			<Image source={{ uri: selectedMeal.imageUrl }}/>
-			<Text>{selectedMeal.title}</Text>
+		<ScrollView style={styles.rootContainer}>
+			<Image source={{ uri: selectedMeal.imageUrl }} style={styles.image}/>
+			<Text style={styles.title}>{selectedMeal.title}</Text>
 
 			<MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity}
-						 affordability={selectedMeal.affordability}/>
+						 affordability={selectedMeal.affordability} textStyle={styles.detailText}/>
 
-			<Text>Ingredients</Text>
-			{selectedMeal.ingredients.map(ingredient => <Text key={ingredient}>{ingredient}</Text>)}
+			<View style={styles.listOuterContainer}>
+				<View style={styles.listContainer}>
+					<Subtitle>Ingredients</Subtitle>
+					<List data={selectedMeal.ingredients}/>
 
-			<Text>Steps</Text>
-			{selectedMeal.steps.map(step => <Text key={step}>{step}</Text>)}
-		</View>
+					<Subtitle>Steps</Subtitle>
+					<List data={selectedMeal.steps}/>
+				</View>
+			</View>
+		</ScrollView>
 	)
 }
+
+const styles = StyleSheet.create({
+	rootContainer: { marginBottom: 32 },
+	image: {
+		width: "100%",
+		height: 300,
+	},
+	title: {
+		margin: 8,
+		fontSize: 24,
+		color: "white",
+		fontWeight: 'bold',
+		textAlign: "center",
+	},
+	detailText: {
+		color: 'white',
+	},
+	listOuterContainer: {
+		alignItems: 'center',
+	},
+	listContainer: {
+		width: "80%",
+	},
+})
 
 export default MealDetailsScreen;
