97134ce63b9efc2c742f7cb84f9e702dd54c4ecf
Adrian <leongadrian36@gmail.com>
2022-10-14 11:37:44 +0800

    pass params to meal item component

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,9 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+	const catId = route.params.categoryId;
+
+	const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
+	function renderMealItem(itemData) {
+		const item = itemData.item;
+		const mealItemProps = {
+			title: item.title,
+			imageUrl: item.imageUrl,
+			duration: item.duration,
+			complexity: item.complexity,
+			affordability: item.affordability,
+		}
+		return <MealItem {...mealItemProps}/>
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text>Meals Overview Screen</Text>
+			<FlatList data={displayedMeals} keyExtractor={item => item.id} renderItem={renderMealItem}/>
 		</View>
 	)
 }
