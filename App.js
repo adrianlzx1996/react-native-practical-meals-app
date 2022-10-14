1a97a9a67b2a42fdf26e8f0392d7ba107b3ac4fa
Adrian <leongadrian36@gmail.com>
2022-10-14 12:06:01 +0800

    meal details screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailsScreen from "./screens/MealDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -32,6 +33,7 @@ export default function App() {
 						//   }
 						// }}
 					/>
+					<Stack.Screen name={"MealDetail"} component={MealDetailsScreen}/>
 				</Stack.Navigator>
 			</NavigationContainer>
 		</>
