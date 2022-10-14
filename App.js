d63794e481d9e480e7ccd27b7cee91ff6d424e32
Adrian <leongadrian36@gmail.com>
2022-10-14 12:28:56 +0800

    add header right button

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
+import { Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<>
-			<StatusBar style="light"/>
-			<NavigationContainer>
+			<StatusBar style="light" />
+			<NavigationContainer >
 				<Stack.Navigator screenOptions={{
 					headerStyle: {
 						backgroundColor: "#351401",
@@ -20,10 +21,10 @@ export default function App() {
 					contentStyle: {
 						backgroundColor: "#3f2f25",
 					},
-				}}>
+				}} >
 					<Stack.Screen name={"MealCategories"} component={CategoriesScreen} options={{
 						title: "All Categories",
-					}}/>
+					}} />
 					<Stack.Screen name={"MealsOverview"} component={MealsOverviewScreen}
 						// options={({ route, navigation }) => {
 						//   const catId = route.params.categoryId;
@@ -33,9 +34,15 @@ export default function App() {
 						//   }
 						// }}
 					/>
-					<Stack.Screen name={"MealDetail"} component={MealDetailsScreen}/>
-				</Stack.Navigator>
-			</NavigationContainer>
+					<Stack.Screen name={"MealDetail"} component={MealDetailsScreen} options={{
+						headerRight: () => {
+							return (
+								<Button title={"Tap Me!"} />
+							)
+						},
+					}} />
+				</Stack.Navigator >
+			</NavigationContainer >
 		</>
 	);
 }
--- screens/MealDetailsScreen.jsx
+++ screens/MealDetailsScreen.jsx
@@ -1,32 +1,47 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import { useLayoutEffect } from "react";
 
-function MealDetailsScreen({ route }) {
+function MealDetailsScreen({ route, navigation }) {
 	const mealId = route.params.mealId;
 
 	const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
+	function headerButtonPressHandler() {
+		console.log("pressed");
+	}
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerRight: () => {
+				return (
+					<Button title={"Tap Me"} onPress={headerButtonPressHandler} />
+				)
+			},
+		})
+	}, [ navigation, headerButtonPressHandler ]);
+
 	return (
-		<ScrollView style={styles.rootContainer}>
-			<Image source={{ uri: selectedMeal.imageUrl }} style={styles.image}/>
-			<Text style={styles.title}>{selectedMeal.title}</Text>
+		<ScrollView style={styles.rootContainer} >
+			<Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+			<Text style={styles.title} >{selectedMeal.title}</Text >
 
 			<MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity}
-						 affordability={selectedMeal.affordability} textStyle={styles.detailText}/>
+						 affordability={selectedMeal.affordability} textStyle={styles.detailText} />
 
-			<View style={styles.listOuterContainer}>
-				<View style={styles.listContainer}>
-					<Subtitle>Ingredients</Subtitle>
-					<List data={selectedMeal.ingredients}/>
+			<View style={styles.listOuterContainer} >
+				<View style={styles.listContainer} >
+					<Subtitle >Ingredients</Subtitle >
+					<List data={selectedMeal.ingredients} />
 
-					<Subtitle>Steps</Subtitle>
-					<List data={selectedMeal.steps}/>
-				</View>
-			</View>
-		</ScrollView>
+					<Subtitle >Steps</Subtitle >
+					<List data={selectedMeal.steps} />
+				</View >
+			</View >
+		</ScrollView >
 	)
 }
 
