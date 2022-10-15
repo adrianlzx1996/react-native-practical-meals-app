eb649f87a04ef92ea03eaba4ec244dd01182baf6
Adrian <leongadrian36@gmail.com>
2022-10-15 14:40:48 +0800

    show favorite meals using redux

--- screens/FavoriteScreen.jsx
+++ screens/FavoriteScreen.jsx
@@ -1,13 +1,14 @@
 import MealsList from "../components/MealsList/MealsList";
-import { useContext } from "react";
-import { FavoritesContext } from "../store/context/favorites-context";
-import { MEALS } from "../data/dummy-data";
 import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import { MEALS } from "../data/dummy-data";
 
 function FavoriteScreen() {
-	const favoriteMealsCtx = useContext(FavoritesContext);
+	// const favoriteMealsCtx = useContext(FavoritesContext);
+	const favoriteMealIds = useSelector(state => state.favoriteMeals.ids);
 
-	const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+	// const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+	const favoriteMeals = MEALS.filter(meal => favoriteMealIds.includes(meal.id));
 
 	if ( favoriteMeals.length === 0 ) {
 		return <View style={styles.rootContainer}>
